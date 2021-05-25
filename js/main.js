e46a0b40bf41105eb2493b93671f59cc575c9175
ksillah <kutah.sillah@gmail.com>
2021-05-25 09:46:40 -0500

    updated

--- js/main.js
+++ js/main.js
@@ -2,35 +2,45 @@ let form = document.querySelector('#weatherDataForm')
 const app_id='ec21147cf5f4da26a3d9077e042adb4a'
 let query_city;
 let query_zip;
+let hide=true;
 
 
 const weatherData = async () => {
     let response;
-    console.log(`zip ${query_zip}`)
-    console.log(`id ${app_id}`)
 
     response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${query_zip}&units=imperial&appid=${app_id}`)
     console.log(response.data)
     document.getElementById('cityName').innerHTML=''
     document.getElementById('cityName').innerHTML=query_city
+
     let max=response.data.main.temp_max + '\u00B0 F';
-    
+    document.getElementsByTagName("table")[0].style.border="thin solid white";
+    document.getElementsByTagName("table")[0].style.color='white'
+    document.getElementById('high header').innerHTML='High'
     document.getElementById('high').innerHTML=''
     document.getElementById('high').append(max)
+
     let min=response.data.main.temp_min + '\u00B0 F';
-    
+    document.getElementsByTagName("table")[1].style.border="thin solid white";
+    document.getElementsByTagName("table")[1].style.color='white'
+    document.getElementById('low header').innerHTML='Low'
     document.getElementById('low').innerHTML=''
     document.getElementById('low').append(min)
+
     let forecast=response.data.weather[0]['main']
-   
+    document.getElementsByTagName("table")[2].style.border="thin solid white";
+    document.getElementsByTagName("table")[2].style.color='white'
+    document.getElementById('forecast header').innerHTML='Forecast'
     document.getElementById('forecast').innerHTML=''
     document.getElementById('forecast').append(forecast)
+
     let humidity=response.data.main.humidity + '%';
-    
+    document.getElementsByTagName("table")[3].style.border="thin solid white";
+    document.getElementsByTagName("table")[3].style.color='white'
+    document.getElementById('humidity header').innerHTML='Humidity'
     document.getElementById('humidity').innerHTML=''
     document.getElementById('humidity').append(humidity)
-
-
+    
 };
 
 const capCity = (cityString) => {
@@ -47,8 +57,6 @@ form.addEventListener('submit', (event) => {
       event.preventDefault();
       query_city = document.querySelector('#city').value;
       query_city=capCity(query_city);
-      console.log(query_city)
       query_zip = document.querySelector('#zip').value;
       let result=weatherData();
-      console.log(result)
 })
